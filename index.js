17c4314f45ed2b373efc05590554e613602ab571
audram-design <69908449+audram-design@users.noreply.github.com>
2021-02-18 18:58:56 -0700

    Trying Mobile

---
+++ index.js
@@ -0,0 +1,19 @@
+function toggleMenu(){
+    var links = document.getElementById("links");
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+  }
+  
+  function resized() {
+  
+    var links = document.getElementById("links");
+
+    if (window.innerWith >= 600) {
+      links.style.display = "block";
+    } else {
+      links.style.display = "none";
+    }
+  }
